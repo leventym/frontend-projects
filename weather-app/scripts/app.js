73ddac061365ceb981a628430e7c364531c03ddc
Levent Yildirim <leventyildirim@icloud.com>
2022-03-17 16:31:19 +0100

    converted functions to a class

--- weather-app/scripts/app.js
+++ weather-app/scripts/app.js
@@ -3,12 +3,13 @@ const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 const time = document.querySelector("img.time");
 const icon = document.querySelector(".icon img");
+const forecast = new Forecast();
 
 const updateUI = (data) => {
   //destructure properties
   const { cityDetails, weather } = data;
 
-  //update detaails template
+  //update details template in the UI
   details.innerHTML = `
     <h5 class="my-3">${cityDetails.EnglishName}</h5>
     <div class="my-3">${weather.WeatherText}</div>
@@ -18,26 +19,17 @@ const updateUI = (data) => {
     </div>
     `;
 
-  //update niight/day & icon image
-
+  //update night/day & icon image
   const iconSource = `img/icons/${weather.WeatherIcon}.svg`;
   icon.setAttribute("src", iconSource);
 
   let timeSource = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
-
   time.setAttribute("src", timeSource);
 
-  //remoce the d-none class if city exits
+  //remove the d-none class if city exits
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
-};
-
-const updateCity = async (city) => {
-  const cityDetails = await getCity(city);
-  const weather = await getWeather(cityDetails.Key);
-
-  return { cityDetails, weather };
 };
 
 cityForm.addEventListener("submit", (e) => {
@@ -48,16 +40,18 @@ cityForm.addEventListener("submit", (e) => {
   cityForm.reset();
 
   //update UI with the city input
-  updateCity(city)
+  forecast
+    .updateCity(city)
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 
   //set local storage
-  localStorage.setItem('city', city);
+  localStorage.setItem("city", city);
 });
 
-if(localStorage.getItem('city')){
-  updateCity(localStorage.getItem('city'))
-  .then(data => updateUI(data))
-  .catch(err => console.log(err))
-};
+if (localStorage.getItem("city")) {
+  forecast
+    .updateCity(localStorage.getItem("city"))
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
+}
--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,33 +1,36 @@
-const key = 'asYR1vouPBrsWDLvHuxRw55envEYujJ5';
-
-//get weather
-const getWeather = async (id) => {
-
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
-
-
-
-//get city information
-const getCity = async (city) => {
-
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
-
-// getCity('sao paulo').then(data => {
-//     return getWeather(data.Key);
-// }).then(data => {
-//     console.log(data);
-// }).catch( err => console.log(err));
+class Forecast {
+    constructor() {
+      this.key = "asYR1vouPBrsWDLvHuxRw55envEYujJ5";
+      this.weatherURI =
+        "http://dataservice.accuweather.com/currentconditions/v1/";
+      this.cityURI =
+        "http://dataservice.accuweather.com/locations/v1/cities/search";
+    }
+    async updateCity(city) {
+      const cityDetails = await this.getCity(city);
+      const weather = await this.getWeather(cityDetails.Key);
+      return { cityDetails, weather };
+    }
+    //get city information
+    async getCity(city) {
+      const query = `?apikey=${this.key}&q=${city}`;
+      const response = await fetch(this.cityURI + query);
+      const data = await response.json();
+      return data[0];
+    }
+    //get weather
+    async getWeather(id) {
+      const query = `${id}?apikey=${this.key}`;
+      const response = await fetch(this.weatherURI + query);
+      const data = await response.json();
+  
+      return data[0];
+    }
+  }
+  
+  // getCity('sao paulo').then(data => {
+  //     return getWeather(data.Key);
+  // }).then(data => {
+  //     console.log(data);
+  // }).catch( err => console.log(err));
+  
